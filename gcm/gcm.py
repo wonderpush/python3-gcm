4bdb12323c281139913f609f537048ebb5aa9f37
Olivier Favre <olivier@wonderpush.com>
2015-06-18 10:42:54 +0200

    Timeout support

--- gcm/gcm.py
+++ gcm/gcm.py
@@ -168,14 +168,20 @@ class GCM(object):
 
         return payload
 
-    def make_request(self, data, is_json=True):
+    def make_request(self, data, is_json=True, **kwargs):
         """
         Makes a HTTP request to GCM servers with the constructed payload
 
         :param data: return value from construct_payload method
+        :param \**kwargs:
+            See below
         :raises GCMMalformedJsonException: if malformed JSON request found
         :raises GCMAuthenticationException: if there was a problem with authentication, invalid api key
-        :raises GCMConnectionException: if GCM is screwed
+        :raises GCMConnectionException: if GCM is screwed or timeout is hit
+
+        :Keyword Arguments:
+            * *timeout* (``float``) --
+              timeout argument passed to requests
         """
 
         # Default Content-Type is
@@ -186,10 +192,19 @@ class GCM(object):
         if not is_json:
             data = urlencode_utf8(data)
 
-        response = self.session.post(
-            self.url, data=data, headers=self.headers,
-            proxies=self.proxy
-        )
+        post_kwargs = {}
+        if 'timeout' in kwargs:
+            post_kwargs['timeout'] = kwargs['timeout']
+
+        try:
+            response = self.session.post(
+                self.url, data=data, headers=self.headers,
+                proxies=self.proxy,
+                **post_kwargs
+            )
+        except requests.exceptions.Timeout as e:
+            raise GCMConnectionException("HTTP call timed out: %s" % e)
+
         # Successful response
         if response.status_code == 200:
             if is_json:
@@ -260,14 +275,20 @@ class GCM(object):
         return []
 
     def plaintext_request(self, registration_id, data=None, collapse_key=None,
-                          delay_while_idle=False, time_to_live=None, retries=5, dry_run=False):
+                          delay_while_idle=False, time_to_live=None, retries=5, dry_run=False, **kwargs):
         """
         Makes a plaintext request to GCM servers
 
         :param registration_id: string of the registration id
         :param data: dict mapping of key-value pairs of messages
+        :param \**kwargs:
+            See below
         :return dict of response body from Google including multicast_id, success, failure, canonical_ids, etc
         :raises GCMMissingRegistrationException: if registration_id is not provided
+
+        :Keyword Arguments:
+            * *timeout* (``float``) --
+              timeout argument passed to requests
         """
 
         if not registration_id:
@@ -278,11 +299,15 @@ class GCM(object):
             delay_while_idle, time_to_live, False, dry_run
         )
 
+        make_request_kwargs = {}
+        if 'timeout' in kwargs:
+            make_request_kwargs['timeout'] = kwargs['timeout']
+
         attempt = 0
         backoff = self.BACKOFF_INITIAL_DELAY
         for attempt in range(retries):
             try:
-                response = self.make_request(payload, is_json=False)
+                response = self.make_request(payload, is_json=False, **make_request_kwargs)
                 return self.handle_plaintext_response(response)
             except GCMUnavailableException:
                 sleep_time = backoff / 2 + random.randrange(backoff)
@@ -293,15 +318,21 @@ class GCM(object):
         raise IOError("Could not make request after %d attempts" % attempt)
 
     def json_request(self, registration_ids, data=None, collapse_key=None,
-                     delay_while_idle=False, time_to_live=None, retries=5, dry_run=False):
+                     delay_while_idle=False, time_to_live=None, retries=5, dry_run=False, **kwargs):
         """
         Makes a JSON request to GCM servers
 
         :param registration_ids: list of the registration ids
         :param data: dict mapping of key-value pairs of messages
+        :param \**kwargs:
+            See below
         :return dict of response body from Google including multicast_id, success, failure, canonical_ids, etc
         :raises GCMMissingRegistrationException: if the list of registration_ids is empty
         :raises GCMTooManyRegIdsException: if the list of registration_ids exceeds 1000 items
+
+        :Keyword Arguments:
+            * *timeout* (``float``) --
+              timeout argument passed to requests
         """
 
         if not registration_ids:
@@ -310,13 +341,17 @@ class GCM(object):
             raise GCMTooManyRegIdsException(
                 "Exceded number of registration_ids")
 
+        make_request_kwargs = {}
+        if 'timeout' in kwargs:
+            make_request_kwargs['timeout'] = kwargs['timeout']
+
         backoff = self.BACKOFF_INITIAL_DELAY
         for attempt in range(retries):
             payload = self.construct_payload(
                 registration_ids, data, collapse_key,
                 delay_while_idle, time_to_live, True, dry_run
             )
-            response = self.make_request(payload, is_json=True)
+            response = self.make_request(payload, is_json=True, **make_request_kwargs)
             info = self.handle_json_response(response, registration_ids)
 
             unsent_reg_ids = self.extract_unsent_reg_ids(info)
